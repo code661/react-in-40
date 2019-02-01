e0b324e95368fbe5b46a1dd65247195c42770f76
tanduzhi <lucky.tanduzhi@gmail.com>
2019-02-01 15:00:12 +0800

    实现简单的组件化

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
+const createDOMFormString = (domString) => {
+  const div = document.createElement('div')
+  div.innerHTML = domString
+  return div
+}
+
 class LikeButton {
+  constructor() {
+    this.state = { isLiked: false }
+  }
   render() {
-    return `
+    this.el = createDOMFormString(`
       <button class="like-btn">
         <span class="like-text">点赞</span>
         <span>👍</span>
       </button>
-  `}
+    `)
+    this.el.addEventListener('click', this.changeLikeText.bind(this), false)
+    return this.el
+  }
+
+  changeLikeText() {
+    const likeText = this.el.querySelector('.like-text')
+    this.state.isLiked = !this.state.isLiked
+    likeText.innerHTML = this.state.isLiked ? '取消' : '点赞'
+  }
 }
 
 let wrapper = document.querySelector('.wrapper')
 const likeButton1 = new LikeButton()
-wrapper.innerHTML = likeButton1.render()
+wrapper.appendChild(likeButton1.render())
 
 const likeButton2 = new LikeButton();
-wrapper.innerHTML += likeButton1.render();
+wrapper.appendChild(likeButton2.render());
